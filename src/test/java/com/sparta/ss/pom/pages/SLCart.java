d215f533276e8617c6bcc1564e0fe734db2f2592
sams4566 <sams4566@gmail.com>
2022-04-08 13:43:46 +0100

    Add tests file

--- src/test/java/com/sparta/ss/pom/pages/SLCart.java
+++ src/test/java/com/sparta/ss/pom/pages/SLCart.java
@@ -2,12 +2,14 @@ package com.sparta.ss.pom.pages;
 
 import org.openqa.selenium.WebDriver;
 
-public class SLProducts {
+public class SLCart {
     private WebDriver driver;
 
-    public SLProducts(WebDriver driver) {
+    public SLCart(WebDriver driver) {
         this.driver = driver;
     }
 
-
+    public String getUrl() {
+        return driver.getCurrentUrl();
+    }
 }
